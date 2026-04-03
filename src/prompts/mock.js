2b231fe06056b1a23723379c3abfcdaa826e217d
yorha2b-lab <266277844+yorha2b-lab@users.noreply.github.com>
2026-04-03 13:23:35 +0800

    fix:修复mock数据格式问题

--- src/prompts/mock.js
+++ src/prompts/mock.js
@@ -1,6 +1,4 @@
 module.exports = (columns, fileName) => `
 根据这些字段生成一个简单的接口mock数据: ${JSON.stringify(columns)},
-格式为{key:value}:
-key: POST /api/${fileName}
-value: {data:模块对象列表},
+格式为{POST /api/${fileName}:{data:模块对象列表}}
 `
