6913be77266b7f597a015e2b5d159f5a605370b5
yorha2b-lab <266277844+yorha2b-lab@users.noreply.github.com>
2026-06-12 10:38:22 +0800

    fix：修复MyImage组件的图片预览问题

--- templates/react/components/MyImage.js
+++ templates/react/components/MyImage.js
@@ -4,7 +4,7 @@ export const MyImage = ({ img, setImg }) => {
     return (
         <div style={{ display: 'none' }}>
             <Image.PreviewGroup preview={{ visible: img.visible, onVisibleChange: vis => setImg({ ...img, visible: vis }) }}>
-                {img.list.map((img, index) => (<Image key={index} src={img} />))}
+                {img.list.map((src, index) => (<Image key={index} src={src} />))}
             </Image.PreviewGroup>
         </div>
     )
